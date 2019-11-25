c6d46aaf092b6ade79662056745b33cb063191a6
Hanna Lindgren <han.lind@gmail.com>
2019-11-25 10:37:45 +0100

    build_and_test.sh -h

--- build_and_test.sh
+++ build_and_test.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ $# -ne 0 ]; then
+    echo "For developers: If you are developing for Wikispeech, and need to make changes to this repository, make sure you run a test build using build_and_test.sh before you make a pull request. Don't run more than one instance of this script at once, and make sure no pronlex server is already running on the default port."
+    exit 0
+fi
+
+
 ./gradlew check
 ./gradlew assembleDist
 ./gradlew test
