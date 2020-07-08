c857c9b661d3165ccefaf1077ed997df53a83f64
kalle <karl.wettin@wikimedia.se>
2020-07-08 17:55:45 +0200

    Introduce Blubber test build
    Blubber test build that starts Mary-TTS and ensure it answers
    successfully on an application version request.
    
    Bug: T257470
    Change-Id: I3b07031a224a9e1294d1db7697179fcf31d76fc5

--- blubber-entrypoint-test.sh
+++ blubber-entrypoint-test.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-# This doesn't actually test Mary TTS.
-# In order to do that we need to execute actual integrations
-# that include having the Mishkal service running.
-# Mary TTS does however come with self tests that can be excuted
-# upon starting the server, but without Mishkal running it will fail.
+# This is the test build entrypoint for Mary-TTS.
+# It will startup the service in the background,
+# wait for 10 seconds and make sure the process didn't die,
+# and then send a version-request to the HTTP server
+# which need to respond with HTTP 200.
+# If all goes well the test will pass.
 
-echo "Test ran successfully"
+echo "Starting Mary TTS STTS."
+export GRADLE_USER_HOME=/srv/gradle_user_home
+cd /srv/mary-tts/build/install/mary-tts/
+./bin/marytts-server &
 
-exit 0
+PID=$!
+sleep 10
+if ! kill -0 ${PID}; then
+  echo "ERROR: Service process has prematurely ended!"
+  exit 1
+fi
+wget -O /dev/null -o /dev/null "http://localhost:59125/version"
+EXIT_CODE=$?
+kill ${PID}
+if [ ${EXIT_CODE} -ne 0 ]; then
+  echo "ERROR: Test failed!"
+else
+  echo "Test successful!"
+fi
+exit ${EXIT_CODE}
